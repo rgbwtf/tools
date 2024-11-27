1183c44563a831096e3c9493676c0f5461b95933
metamonk <metamonk@ratlabs.xyz>
2024-11-27 03:14:58 -0600

    feat: invert binary values in signature

--- app/api/uploadthing/route.ts
+++ app/api/uploadthing/route.ts
@@ -3,10 +3,10 @@ import { ourFileRouter } from "./core";
 import { env } from '@/env.mjs';
 
 // Export routes for Next App Router
-export const { GET, POST } = createNextRouteHandler({
-  router: ourFileRouter,
-  config: {
-    uploadthingId: env.UPLOADTHING_APP_ID,
-    uploadthingSecret: env.UPLOADTHING_SECRET,
-  },
-});
+// export const { GET, POST } = createNextRouteHandler({
+//   router: ourFileRouter,
+//   config: {
+//     uploadthingId: env.UPLOADTHING_APP_ID,
+//     uploadthingSecret: env.UPLOADTHING_SECRET,
+//   },
+// });
